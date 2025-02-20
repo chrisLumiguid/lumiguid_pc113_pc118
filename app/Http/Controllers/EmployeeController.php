988f9660abd7001290d698489259511a02aa42fe
chrisLumiguid <c.lumiguid@mlgcl.edu.ph>
2025-02-21 01:02:11 +0800

    2-20-2025 Activity 2

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -5,10 +5,35 @@ namespace App\Http\Controllers;
 use App\Models\Employee;
 use Illuminate\Http\Request;
 
+
+// This is the original code for Activity 1
+
+// class EmployeeController extends Controller
+// {
+//     public function index()
+//     {
+//         return response()->json(Employee::all());
+//     }
+// }
+
+
+// This is the code for Activity 2
 class EmployeeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return response()->json(Employee::all());
+        $query = Employee::query();
+
+        if ($request->has('search')) {
+            $search = $request->input('search');
+            $query->where('f_name', 'LIKE', "%{$search}%")
+                  ->orWhere('l_name', 'LIKE', "%{$search}%")
+                  ->orWhere('email', 'LIKE', "%{$search}%")
+                  ->orWhere('phone', 'LIKE', "%{$search}%");
+        }
+
+        $employees = $query->get();
+
+        return response()->json($employees);
     }
 }
--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -5,10 +5,32 @@ namespace App\Http\Controllers;
 use App\Models\Student;
 use Illuminate\Http\Request;
 
+// This is the original code for Activity 1
+
+// class StudentController extends Controller
+// {
+//     public function index()
+//     {
+//         return response()->json(Student::all());
+//     }
+// }
+
 class StudentController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return response()->json(Student::all());
+        $query = Student::query();
+
+        if ($request->has('search')) {
+            $search = $request->input('search');
+            $query->where('f_name', 'LIKE', "%{$search}%"); 
+                //   ->orWhere('l_name', 'LIKE', "%{$search}%")
+                //   ->orWhere('email', 'LIKE', "%{$search}%")
+                //   ->orWhere('phone', 'LIKE', "%{$search}%");
+        }
+
+        $students = $query->get();
+
+        return response()->json($students);
     }
 }
